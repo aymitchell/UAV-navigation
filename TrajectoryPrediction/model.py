757c1a249b62d6c05768e0a203f094272f6934b0
Alanna Mitchell <aymitchell@users.noreply.github.com>
2021-05-03 20:00:55 -0400

    update  model architecture and training file

--- TrajectoryPrediction/model.py
+++ TrajectoryPrediction/model.py
@@ -21,38 +21,54 @@ Defines the SocialLSTM model.
 class SocialLSTM(nn.Module):
     def __init__(self):
         super(SocialLSTM, self).__init__()
-        self.hsize = 128
-        self.embed_r = nn.Linear(in_features=2, out_features=64)
-        self.embed_e = nn.Linear(in_features=self.hsize, out_features=self.hsize*2) 
-        self.lstm1 = nn.LSTMCell(input_size=64+self.hsize*2, hidden_size=self.hsize)
-        self.predict_mean = nn.Linear(in_features=self.hsize+64, out_features=64)
-        self.predict_mean2 = nn.Linear(in_features=64+2, out_features=32)
-        self.predict_mean3 = nn.Linear(in_features=32, out_features=2)
+        self.hsize = 256
+        self.esize = 32
+        self.rsize = 64
+        self.embed_r = nn.Linear(in_features=2, out_features=self.rsize)
+        self.embed_e = nn.Linear(in_features=self.hsize, out_features=self.esize) 
+        self.lstm1 = nn.LSTMCell(input_size=(self.esize*16)+self.rsize, hidden_size=self.hsize)
+        self.predict = nn.Linear(in_features=self.hsize+self.rsize, out_features=64)
+        self.predict2 = nn.Linear(in_features=64, out_features=32)
+        self.predict_mean = nn.Linear(in_features=32, out_features=2)
+        self.predict_stddev = nn.Linear(in_features=32, out_features=2)
+        self.predict_corr = nn.Linear(in_features=32, out_features=1)
         
         self.dropout = nn.Dropout(p=0.01)
     
     def social(self, data, hiddens):
         '''
         data : [batch, agents, 2]
-        hiddens : [batch, agents 128]
+        hiddens : [batch, agents, 128]
         '''
+        batch, agents = data.shape[0], data.shape[1]
         dist = 32
+        pad = int(dist/2)
+        step = int(dist/4)
         inactive = (data[:,:,0] < 0)
-        distances = []
+        xmin = torch.tensor([step,0,-step,-2*step,step,0,-step,-2*step,step,0,-step,-2*step,step,0,-step,-2*step]).reshape(16,1,1).to(device)
+        xmax = torch.tensor([2*step,step,0,-step,2*step,step,0,-step,2*step,step,0,-step,2*step,step,0,-step]).reshape(16,1,1).to(device)
+        ymin = torch.tensor([-2*step,-2*step,-2*step,-2*step,-step,-step,-step,-step,0,0,0,0,step,step,step,step]).reshape(16,1,1).to(device)
+        ymax = torch.tensor([-step,-step,-step,-step,0,0,0,0,step,step,step,step,2*step,2*step,2*step,2*step]).reshape(16,1,1).to(device)
+        H = []
+        masks = []
         for b in range(data.shape[0]):
-            distances.append(data[b].unsqueeze(1)-data[b])
-        distances = torch.sqrt(torch.sum(pow(torch.stack(distances,dim=0),2),dim=-1)) # [batch, agent, agent, 2]
-        mask = (distances < dist).float()
-        mask[inactive.unsqueeze(-1).repeat(1,1,data.shape[1])] = 0
-        mask[inactive.unsqueeze(-2).repeat(1,data.shape[1],1)] = 0
-        mask = mask * (torch.diag(torch.ones(data.shape[1])).repeat(data.shape[0],1,1).to(device) < 1).float()
-        H = torch.bmm(mask,hiddens)
+            # [quad, agents, agents]
+            mask = (((data[b].unsqueeze(1)-data[b])[:,:,0]).unsqueeze(0).repeat(16,1,1) >= xmin) * \
+                   (((data[b].unsqueeze(1)-data[b])[:,:,0]).unsqueeze(0).repeat(16,1,1) <= xmax) * \
+                   (((data[b].unsqueeze(1)-data[b])[:,:,1]).unsqueeze(0).repeat(16,1,1) >= ymin) * \
+                   (((data[b].unsqueeze(1)-data[b])[:,:,1]).unsqueeze(0).repeat(16,1,1) <= ymax)
+            
+            inact = inactive[b].unsqueeze(0).repeat(16,1) # [quad,agents]
+            mask[inact.unsqueeze(-1).repeat(1,1,data.shape[1])] = 0
+            mask[inact.unsqueeze(-2).repeat(1,data.shape[1],1)] = 0
+            mask = mask * (torch.diag(torch.ones(data.shape[1])) < 1).unsqueeze(0).repeat(16,1,1).to(device).float()
+            H.append(torch.bmm(mask,hiddens[b].unsqueeze(0).repeat(16,1,1)))#.cpu())
+        H = torch.stack(H, dim=0)#.to(device)
         return H
-    
+            
     def forward(self, data, tprob):
         '''
         data : [batch, agents, frames, 2]
-        tprob : teacher forcing probability
         '''
         data = data.to(device)
         maxlen = data.shape[2]
@@ -61,31 +77,42 @@ class SocialLSTM(nn.Module):
         if tprob == 1:
             teach_prob = np.zeros((maxlen))
             for i in range(0,maxlen,20):
-                teach_prob[i+8:i+20] = 1
+                if i > (maxlen-20): end = -1
+                else: end = i+20
+                teach_prob[i+8:end] = 1
         else:
             teach_prob = np.random.binomial(n=1,p=tprob,size=maxlen)
             teach_prob[:8] = 0
+
         hiddens1 = torch.zeros((batch*agents,self.hsize)).to(device)
-        means = []
+        states1 = torch.zeros((batch*agents,self.hsize)).to(device)
+        means, stddevs, corrs = [], [], []
         for t in range(maxlen):
-            if teach_prob[t] < 1:
-                xin = data[:,:,t,:]
+            if teach_prob[t]:
+                xin = mean.masked_fill_((data[:,:,t,:] < 0),-1)
             else:
-                xin = mean
+                xin = data[:,:,t,:]
             
             r = F.relu(self.embed_r(xin))
             r = self.dropout(r)
             H = self.social(xin, hiddens1.reshape(batch,agents,self.hsize))
             e = F.relu(self.embed_e(H))
+            e = e.reshape(batch, agents, -1)
             e = self.dropout(e)
-            inp = torch.cat([r,e],dim=-1).reshape(-1,64+self.hsize*2)
-            hiddens1, _ = self.lstm1(inp)
+            inp = torch.cat([r,e],dim=-1).reshape(-1,self.rsize+(self.esize*16))
+            hiddens1, states1 = self.lstm1(inp, (hiddens1,states1))
             
             inp2 = torch.cat([r,hiddens1.reshape(data.shape[0],data.shape[1],self.hsize)],dim=-1)
-            mean = F.relu(self.predict_mean(inp2))
-            mean = self.predict_mean2(torch.cat([mean,xin],dim=-1))
-            mean = self.predict_mean3(mean)
+            out = self.predict(inp2)
+            out = self.predict2(out)
+            mean = self.predict_mean(out)
+            stddev = torch.exp(self.predict_stddev(out))
+            corr = torch.tanh(self.predict_corr(out))
             means.append(mean)
+            stddevs.append(stddev)
+            corrs.append(corr)
             
         means = torch.stack(means, dim=2)
-        return means
+        stddevs = torch.stack(stddevs, dim=2)
+        corrs = torch.stack(corrs, dim=2)
+        return means, stddevs, corrs
--- TrajectoryPrediction/train.py
+++ TrajectoryPrediction/train.py
@@ -24,6 +24,7 @@ Train the model.
 '''
 
 def train(model, criterion, optimizer, trainloader, valloader, startepoch, nepochs, save):
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     print('Training...')
     print(time.ctime())
     torch.autograd.set_detect_anomaly(True)
@@ -31,47 +32,22 @@ def train(model, criterion, optimizer, trainloader, valloader, startepoch, nepoc
         print('Starting Epoch %d:' %(epoch), time.ctime())
         start_epoch = time.time()
         model.train()
-        if epoch < 40: tprob = 0.1
-        if epoch >= 40: tprob = 0.15
-        if epoch >= 60: tprob = 0.2
-        if epoch >= 100: tprob = 0.3
-        if epoch >= 140: tprob = 0.4
-        if epoch >= 180: tprob = 0.5
-        if epoch >= 220: tprob = 0.55
-        if epoch >= 260: tprob = 0.6
-        if epoch >= 300: tprob = 0.65
-        if epoch >= 340: tprob = 0.7
-        if epoch >= 380: tprob = 0.75
-        if epoch >= 420: tprob = 0.8
-        if epoch >= 460: tprob = 0.85
-        if epoch >= 500: tprob = 0.9
-        
-        if epoch >= 120: optimizer.param_groups[0]['lr'] = 5e-3
-        if epoch >= 200: optimizer.param_groups[0]['lr'] = 1e-3
-        if epoch >= 280: optimizer.param_groups[0]['lr'] = 5e-4
-        if epoch >= 360: optimizer.param_groups[0]['lr'] = 1e-4
-        if epoch >= 400: optimizer.param_groups[0]['lr'] = 5e-5
-        if epoch >= 440: optimizer.param_groups[0]['lr'] = 1e-5
-        if epoch >= 520: optimizer.param_groups[0]['lr'] = 5e-6
-
-
         train_loss, train_dist = [], []
         for batch_num, (data_in) in enumerate(trainloader):
+            data_in = data_in.to(device)
             start_batch = time.time()
-            
-            means, stddevs, corrs = [], [], []
-            
+                        
             optimizer.zero_grad()
-            means = model(data_in, tprob)
+            means, stddevs, corrs = model(data_in, tprob)
 
             mask = (data_in[:,:,1:,:] < 0)
-            
-            loss = criterion(means[:,:,:-1,:], data_in[:,:,1:,:].to(device))
-            loss = loss.masked_fill_(mask.to(device),0).sum()
+            loss = criterion(data_in[:,:,1:,:].to(device), means[:,:,:-1,:],\
+                             stddevs[:,:,:-1,:], corrs[:,:,:-1,:], mask[:,:,:,0].to(device))
+            loss = loss.sum()
             loss = (loss / (mask < 1).sum()).clamp(max=1e18)
             loss.backward()
             
-            nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+            nn.utils.clip_grad_norm_(model.parameters(), 10.0)
             optimizer.step()
             
             dist = calc_dist(data_in[:,:,1:,:].detach().cpu(), means[:,:,:-1,:].detach().cpu(),\
@@ -81,7 +57,7 @@ def train(model, criterion, optimizer, trainloader, valloader, startepoch, nepoc
             torch.cuda.empty_cache()
             train_loss.append(loss.detach().cpu().item())
             train_dist.append(dist.item())
-            del loss, mask, means, data_in
+            del loss, mask, means, stddevs, corrs, data_in
 
             if epoch == 0 and batch_num == 0: 
                 print('Single Batch Time: %d min %d sec' %((time.time()-start_batch)//60, (time.time()-start_batch)%60)) 
@@ -105,18 +81,20 @@ def train(model, criterion, optimizer, trainloader, valloader, startepoch, nepoc
         print("==============================================")
 
 def validate(model, criterion, valloader):
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     model.eval()
     val_loss, val_dist = [], []
     for batch_num, (data_in) in enumerate(valloader):
-        train_loss, train_dist = [], []
+        data_in = data_in.to(device)
+
         means, stddevs, corrs = [], [], []
 
-        means = model(data_in, tprob=1)
+        means, stddevs, corrs = model(data_in, tprob=1)
 
         mask = (data_in[:,:,1:,:] < 0)
-
-        loss = criterion(means[:,:,:-1,:], data_in[:,:,1:,:].to(device))
-        loss = loss.masked_fill_(mask.to(device),0).sum()
+        loss = criterion(data_in[:,:,1:,:].to(device), means[:,:,:-1,:],\
+                         stddevs[:,:,:-1,:], corrs[:,:,:-1,:], mask[:,:,:,0].to(device))
+        loss = loss.sum()
         loss = loss / (mask < 1).sum()
 
         dist = calc_dist(data_in[:,:,1:,:].detach().cpu(), means[:,:,:-1,:].detach().cpu(),\
@@ -126,9 +104,23 @@ def validate(model, criterion, valloader):
         torch.cuda.empty_cache()
         val_loss.append(loss.detach().cpu().item())
         val_dist.append(dist.item())
-        del loss, mask, means, data_in
+        del loss, mask, means, stddevs, corrs, data_in
     
-    return np.mean(val_loss), np.mean(val_dist)
+    return np.mean(val_loss), np.mean(val_dist), np.mean(val_evaldist)#, np.mean(val_finaldist)
+
+def nllLoss(target, mean, stddev, corr, mask):
+    x1, x2 = target[:,:,:,0], target[:,:,:,1]
+    m1, m2 = mean[:,:,:,0], mean[:,:,:,1]
+    std1, std2 = stddev[:,:,:,0].clamp(min=1e-6), stddev[:,:,:,1].clamp(min=1e-6)
+    corr = corr[:,:,:].squeeze(-1)
+    
+    Z = pow((x1-m1)/std1,2) + pow((x2-m2)/std2,2) - 2*corr*(((x1-m1)*(x2-m2))/(std1*std2))
+    N = (1 / (2*np.pi*std1*std2*torch.sqrt(1-pow(corr,2).clamp(max=1-1e-6))))
+    a = torch.log(N.clamp(min=1e-6))
+    b = (-Z/(2*(1-pow(corr,2))))
+    L = a + b
+    L = L.masked_fill_(mask,0)
+    return -L
 
 def run(startepoch, nepochs, modelfilename, optfilename, save):
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -143,16 +135,14 @@ def run(startepoch, nepochs, modelfilename, optfilename, save):
     # define the model
     model = SocialLSTM()
     if modelfilename is not None:
-        model.load_state_dict(state_dict_helper(modelfilename))
-    model = nn.DataParallel(model)
-    # model = model.to(torch.cuda.current_device())
+        model.load_state_dict(modelfilename, map_location=device)
     model = model.to(device)
 
     # define the criterion and optimizer
-    criterion = nn.L1Loss(reduction='none')
+    criterion = nllLoss
     learning_rate = 3e-2
     weight_decay = 5e-5
-    # optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
     if optfilename is not None:
         optimizer.load_state_dict(torch.load(optfilename))
 
@@ -170,6 +160,6 @@ if __name__ == "__main__":
     save = False
 
     startepoch = 0
-    nepochs = 50
+    nepochs = 100
 
     run(startepoch, nepochs, modelfilename, optfilename, save)
